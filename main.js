e4577ddcab02699a0633c2ae624ad883305423de
Evelina Norlin <evelina.norlin@hotmail.com>
2023-03-01 14:08:33 +0100

    commited form

---
+++ main.js
@@ -0,0 +1,10 @@
+// API ska hantera inloggnongen
+// frontend med inloggningsformulär
+// Formulär ska skicka användarnamn och lösenord till server
+//Global array på servern
+// alla användare ska ha unikt ID
+// Servern ska kolla om det är en korrekt inloggning, i så fall svara med användarens ID, spara detta i localStorage på klienten.
+// Vid felaktig inloggning skall en error skickas tillbaka.
+
+// Två formulär (logga in, skapa användare)
+
