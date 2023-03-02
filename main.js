694795d25248244df81ea523def91125f8065f90
Evelina Norlin <evelina.norlin@hotmail.com>
2023-03-02 10:57:47 +0100

    built the project

--- main.js
+++ main.js
@@ -8,3 +8,78 @@
 
 // Två formulär (logga in, skapa användare)
 
+const userList = document.getElementById('user-list');
+const saveUserBtn = document.getElementById('newUserBtn');
+const newUser = document.getElementById('newUser');
+const newPassword = document.getElementById('newPassword');
+const loginUser = document.getElementById('loginUser');
+const loginPassword = document.getElementById('loginPassword');
+const loginBtn = document.getElementById('loginBtn');
+const loginMessage = document.getElementById('loginMessage');
+
+fetch("http://localhost:3000/users")
+  .then(res => res.json())
+  .then(data => {
+    printUsers(data);
+  })
+
+function printUsers(users){
+  userList.innerHTML = ``;
+
+  users.map(user => {
+    let li = document.createElement("li");
+    li.id = user.id;
+    li.innerHTML = user.username + " " + user.password;
+
+    userList.appendChild(li)
+  })
+}
+
+saveUserBtn.addEventListener('click', () => {
+  let user = {username: newUser.value, password: newPassword.value}
+  
+  fetch("http://localhost:3000/users/newuser", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user)
+  })
+    .then(res => res.json())
+    .then(data => printUsers(data))
+})
+
+loginBtn.addEventListener('click', () => {
+  let userLogin = {username: loginUser.value, password: loginPassword.value}
+
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(userLogin)
+  })
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      console.log(data.message.username)
+
+      if(data.message.id){
+        loginMessage.innerText = `Du är inloggad`;
+      } else{
+        loginMessage.innerText = `Fel användarnamn eller lösenord`;
+      }
+    })
+})
+  // fetch("http://localhost:3000/users")
+  // .then(res => res.json())
+  // .then(data => {
+  //   console.log(data);
+  //   for (let i = 0; i < data.length; i++){
+  //     if(loginUser.value === data[i].username && loginPassword.value === data[i].password){
+  //       console.log("succes!")
+  //     }
+  //   }
+  // })
+
+
